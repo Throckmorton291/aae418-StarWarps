962093e444f006d59b46e3bc16e7a3e35f5a6a0f
Throckmorton291 <108034540+Throckmorton291@users.noreply.github.com>
2023-03-31 10:17:48 -0400

    Update starwarps_flight_code.py

--- starwarps_flight_code.py
+++ starwarps_flight_code.py
@@ -6,16 +6,31 @@ GPIO.setmode(GPIO.BOARD)
 
 #Pin definitions
 led = 37
-actuator = 33
+actuator = 31
+PWMpin = 32
 
 #Set up pins as outputs
 GPIO.setup(led,GPIO.OUT)
 GPIO.setup(actuator,GPIO.OUT)
+pi_pwm = GPIO.PWM(PWMpin,1000)
+
+#Start PWM duty cycle
+pi_pwm.start(0)
 
 #Turn on the LED
 GPIO.output(led,True)
+while True:
+    for duty in range(0,101,1):
+        pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
+        sleep(0.01)
+    sleep(0.5)
+    
+    for duty in range(100,-1,-1):
+        pi_pwm.ChangeDutyCycle(duty)
+        sleep(0.01)
+    sleep(0.5)
 
 #Run the linear actuator
-GPIO.output(actuator,True)
-time.sleep(1)
-GPIO.output(actuator,False)
+#GPIO.output(actuator,True)
+#time.sleep(1)
+#GPIO.output(actuator,False)
